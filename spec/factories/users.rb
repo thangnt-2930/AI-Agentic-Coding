10ed0da07f5d5112fe50da21e563a8e3539b0861
thangnt-2930 <nguyen.tan.thang@sun-asterisk.com>
2026-03-16 17:32:26 +0700

    T042-T043: Transaction filter and index view

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { "password" }
-    password_confirmation { "password" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
