1edf03f9246bedc61b22381860aef4f06b1b2350
thangnt-2930 <nguyen.tan.thang@sun-asterisk.com>
2026-03-16 13:45:19 +0700

    T012: Add Devise mailer default_url_options and Bullet config to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,15 @@ Rails.application.configure do
   # Set localhost to be used by links generated in mailer templates.
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  # Bullet gem configuration (for N+1 query detection)
+  config.after_initialize do
+    if defined?(Bullet)
+      Bullet.enable = true
+      Bullet.rails_logger = true
+      Bullet.raise = true
+    end
+  end
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
